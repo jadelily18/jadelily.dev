4c4ffb08bdf4ff250a741c8b6db6614d9831ae12
Jade Nash <jade.nashh18@gmail.com>
2022-11-25 17:44:34 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+    <title>Jade</title>
+</svelte:head>
+<main>
+    <a
+        href="https://github.com/jadelily18/jadelily.dev"
+        class="hover-underline-animation"
+    >
+        <h1 class="title">jadelily.dev</h1>
+    </a>
+    <p>is under construction...</p>
+</main>
+
+<style>
+    .title {
+        margin: 0;
+    }
+</style>
