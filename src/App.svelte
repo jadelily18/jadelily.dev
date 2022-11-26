e6fa770a486b554e5e516a81602f04b135983e59
Jade Nash <jade.nashh18@gmail.com>
2022-11-26 11:54:23 -0600

    layout + scss + link to git commit

--- src/App.svelte
+++ src/App.svelte
@@ -8,26 +8,47 @@
 <svelte:head>
     <title>Jade</title>
 </svelte:head>
-<main class="main">
-    <a
-        href="https://github.com/jadelily18/jadelily.dev"
-        class="hover-underline-animation"
-    >
-        <h1 class="title">jadelily.dev</h1>
-    </a>
-    <p>is under construction...</p>
-    <p>
-        {repo_owner}/{repo_slug}
-        {repo_branch}@{commit_sha.substring(0, 7)}
-    </p>
-</main>
+<body>
+    <div />
+    <main>
+        <a
+            href="https://github.com/jadelily18/jadelily.dev"
+            class="hover-underline-animation"
+        >
+            <h1 class="title">jadelily.dev</h1>
+        </a>
+        <p>is under construction...</p>
+    </main>
+    <footer>
+        <p>
+            {repo_owner}/{repo_slug}
+            {repo_branch}@<a
+                class="link"
+                href="https://github.com/{repo_owner}/{repo_slug}/tree/{commit_sha}"
+                >{commit_sha.substring(0, 7)}</a
+            >
+        </p>
+    </footer>
+</body>
 
-<style>
-    /* .main {
-        height: screen;
-    } */
+<style lang="scss">
+    body {
+        display: flex;
+        flex-direction: column;
+        width: 100vw;
+        text-align: center;
+        justify-content: space-between;
+        align-content: center;
+
+        --primary_color: #fb7bff;
+    }
 
     .title {
         margin: 0;
+    }
+
+    .link {
+        color: var(--primary_color);
+        text-decoration: underline;
     }
 </style>
