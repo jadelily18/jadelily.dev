c36f914d15bf3e60a02e924b2111db1ee4e0790b
Jade Nash <jade.nashh18@gmail.com>
2022-11-25 18:43:23 -0600

    test vercel env variables

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
 <svelte:head>
     <title>Jade</title>
 </svelte:head>
-<main>
+<main class="main">
     <a
         href="https://github.com/jadelily18/jadelily.dev"
         class="hover-underline-animation"
@@ -12,9 +12,22 @@
         <h1 class="title">jadelily.dev</h1>
     </a>
     <p>is under construction...</p>
+    <p>
+        {process.env.VITE_VERCEL_GIT_REPO_OWNER}/{process.env
+            .VITE_VERCEL_GIT_REPO_SLUG}
+        {process.env
+            .VITE_VERCEL_GIT_COMMIT_REF}@{process.env.VITE_VERCEL_GIT_COMMIT_SHA.substring(
+            0,
+            7
+        )}
+    </p>
 </main>
 
 <style>
+    /* .main {
+        height: screen;
+    } */
+
     .title {
         margin: 0;
     }
