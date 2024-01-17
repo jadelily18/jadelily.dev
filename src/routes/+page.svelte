40f2986d9e3247fca153da862ba61b965ef3b1be
Jade Nash <jade@lilydev.com>
2024-01-16 20:40:53 -0600

    Add meta info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
+	import { MetaTags } from 'svelte-meta-tags';
+
 	import { title } from '$lib/stores/title';
 
 	title.set('home');
 </script>
+
+<MetaTags
+	title={$title}
+	description="Hi, I'm Jade! I'm a software developer working on lots of cool open source projects!"
+	openGraph={{
+		url: 'https://jadelily.dev',
+		title: $title,
+		description:
+			"Hi, I'm Jade! I'm a software developer working on lots of cool open source projects!",
+		siteName: 'jadelily.dev'
+	}}
+	additionalMetaTags={[
+		{
+			name: 'theme-color',
+			content: '#f5c2e7'
+		}
+	]}
+/>
 
 <div class="flex flex-col w-full place-items-center place-self-center">
 	<h1>Welcome to SvelteKit</h1>
