b0a47535592e14c7092556a40fbf8e9d3b03bbed
Jade Nash <jade@lilydev.com>
2024-01-16 20:20:28 -0600

    Fix styles for navbar

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -7,23 +7,18 @@
 	import { darkMode } from '$lib/stores/theme';
 
 	let contextMenuOpen: boolean = false;
-
-	let buttonStyles =
-		'shadow-none dark:shadow-none bg-transparent dark:bg-transparent hover:bg-gray-300 hover:dark:bg-zinc-800';
 </script>
 
 <div class="flex justify-between place-items-center">
-	<Button class="text-xl font-semibold btn-nav w-auto px-3 {buttonStyles}" href="/"
-		>jadelily.dev</Button
-	>
+	<Button class=" btn text-xl font-semibold w-auto px-3" href="/">jadelily.dev</Button>
 	<div class="flex justify-center w-full gap-3">
-		<Button class="w-24 {buttonStyles}" href="/">About</Button>
-		<Button class="w-24 {buttonStyles}" href="/social">Socials</Button>
-		<Button class="w-24 {buttonStyles}" href="/portfolio">Portfolio</Button>
-		<Button class="w-24 {buttonStyles}" href="/blog">Blog</Button>
+		<Button class="btn w-24" href="/">About</Button>
+		<Button class="btn w-24" href="/social">Socials</Button>
+		<Button class="btn w-24" href="/portfolio">Portfolio</Button>
+		<Button class="btn w-24" href="/blog">Blog</Button>
 	</div>
 	<div class="relative">
-		<Button class={buttonStyles} on:click={() => (contextMenuOpen = !contextMenuOpen)}>
+		<Button class="btn" on:click={() => (contextMenuOpen = !contextMenuOpen)}>
 			<Menu />
 		</Button>
 		{#if contextMenuOpen}
@@ -47,8 +42,8 @@
 				></div>
 				<a
 					class="
-						flex justify-center place-items-center gap-2 py-1 px-2 rounded-md
-						transition-colors {buttonStyles}
+						btn flex justify-center place-items-center gap-2
+						py-1 px-2 rounded-md transition-colors
 					"
 					href="https://github.com/jadelily18/jadelily.dev"
 				>
