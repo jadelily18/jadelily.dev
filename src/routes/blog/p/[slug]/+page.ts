763b8ad167985f9eeae0f27f218c8363f80365de
Jade Nash <jade@lilydev.com>
2024-01-26 02:11:02 -0600

    Attempt to fix deployment not finding MD files

--- src/routes/blog/p/[slug]/+page.ts
+++ src/routes/blog/p/[slug]/+page.ts
@@ -2,8 +2,8 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
 	try {
-		// const post = await import(`../../../../posts/${params.slug}.md`);
-		const post = await import(`/src/posts/${params.slug}.md`);
+		const post = await import(`../../../../posts/${params.slug}.md`);
+		// const post = await import(`/src/posts/${params.slug}.md`);
 
 		return {
 			content: post.default,
