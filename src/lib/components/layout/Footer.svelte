34dd3319ec0c9b8bb57ce1b8b7179c2be53aea9d
Jade Nash <jade@lilydev.com>
2024-01-28 15:07:23 -0600

    Make year in footer dynamic

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,3 +1,5 @@
 <div class="w-full flex justify-center">
-	<p><a class="link" href="/license">MIT License</a> &copy; 2023 Jade Nash</p>
+	<p>
+		<a class="link" href="/license">MIT License</a> &copy; {new Date().getFullYear()} Jade Nash
+	</p>
 </div>
