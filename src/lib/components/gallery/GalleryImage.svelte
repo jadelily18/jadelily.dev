d9f59d2c4ed9f736cf949b3b11cb34b97f6c1f4a
Jade <jade@lilydev.com>
2026-07-29 20:25:00 -0500

    remove attribution from gallery grid items

--- src/lib/components/gallery/GalleryImage.svelte
+++ src/lib/components/gallery/GalleryImage.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { cn } from "$lib/shadcn/utils";
 	import type { GalleryItem } from "$types/art";
-	import { Attribution } from "@components/image";
 	import type { ButtonProps } from "@uilib/button";
 
 	type Props = {
@@ -21,14 +20,6 @@
 	)}
 	{onclick}
 >
-	<Attribution class="mb-1.5 mr-1.5 z-10">
-		{#snippet content(contentStyles)}
-			<a
-				class={cn("hover:underline", contentStyles)}
-				href={item.attribution.url}>{item.attribution.label}</a
-			>
-		{/snippet}
-	</Attribution>
 	<img
 		class="w-full h-full cursor-pointer object-cover"
 		style="object-position: {item.offset?.x ?? 50}% {item.offset?.y ?? 50}%"
